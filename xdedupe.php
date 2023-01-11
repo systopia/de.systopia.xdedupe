1713a8b99db1e21139ed22da3b70f12c74235334
B. Endres <endres@systopia.de>
2023-01-11 10:01:16 -0600

    civix upgrade

--- xdedupe.php
+++ xdedupe.php
@@ -45,16 +45,6 @@ function xdedupe_civicrm_alterLogTables(&$logTableSpec)
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function xdedupe_civicrm_xmlMenu(&$files)
-{
-    _xdedupe_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -112,58 +102,6 @@ function xdedupe_civicrm_disable()
 function xdedupe_civicrm_upgrade($op, CRM_Queue_Queue $queue = null)
 {
     return _xdedupe_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function xdedupe_civicrm_managed(&$entities)
-{
-    _xdedupe_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function xdedupe_civicrm_caseTypes(&$caseTypes)
-{
-    _xdedupe_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function xdedupe_civicrm_angularModules(&$angularModules)
-{
-    _xdedupe_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function xdedupe_civicrm_alterSettingsFolders(&$metaDataFolders = null)
-{
-    _xdedupe_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
