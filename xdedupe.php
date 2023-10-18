3a171f16470255a684171c9515fb9d97feb60115
B. Endres <endres@systopia.de>
2023-10-18 21:22:45 +0200

    create pre CiviCRM 5.38 version

--- xdedupe.php
+++ xdedupe.php
@@ -51,7 +51,7 @@ function xdedupe_civicrm_alterLogTables(&$logTableSpec)
  */
 function xdedupe_civicrm_xmlMenu(&$files)
 {
-  _xdedupe_civix_civicrm_xmlMenu($files);
+    _xdedupe_civix_civicrm_xmlMenu($files);
 }
 
 /**
@@ -65,6 +65,26 @@ function xdedupe_civicrm_install()
 }
 
 /**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function xdedupe_civicrm_postInstall()
+{
+    _xdedupe_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function xdedupe_civicrm_uninstall()
+{
+    _xdedupe_civix_civicrm_uninstall();
+}
+
+/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
@@ -72,6 +92,90 @@ function xdedupe_civicrm_install()
 function xdedupe_civicrm_enable()
 {
     _xdedupe_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function xdedupe_civicrm_disable()
+{
+    _xdedupe_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function xdedupe_civicrm_upgrade($op, CRM_Queue_Queue $queue = null)
+{
+    return _xdedupe_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function xdedupe_civicrm_managed(&$entities)
+{
+    _xdedupe_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function xdedupe_civicrm_caseTypes(&$caseTypes)
+{
+    _xdedupe_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function xdedupe_civicrm_angularModules(&$angularModules)
+{
+    _xdedupe_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function xdedupe_civicrm_alterSettingsFolders(&$metaDataFolders = null)
+{
+    _xdedupe_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
+ */
+function xdedupe_civicrm_entityTypes(&$entityTypes)
+{
+    _xdedupe_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
@@ -92,23 +196,11 @@ function xdedupe_civicrm_enable()
  */
 function xdedupe_civicrm_navigationMenu(&$menu)
 {
-    // add automation section
-    if (!_xdedupe_menu_exists($menu, 'Administer/automation')) {
-        _xdedupe_civix_insert_navigation_menu($menu, 'Administer', [
-            'label' => E::ts('Automation'),
-            'name' => 'automation',
-            'url' => NULL,
-            'permission' => 'administer CiviCRM',
-            'operator' => NULL,
-            'separator' => 0,
-        ]);
-    }
-
     _xdedupe_civix_insert_navigation_menu(
         $menu,
         'Administer/automation',
         [
-            'label'      => E::ts('Extended Deduplication (X-Dedupe)'),
+            'label'      => E::ts('Extended Deduplication'),
             'name'       => 'xdedupe_manage',
             'url'        => 'civicrm/xdedupe/manage',
             'permission' => 'administer CiviCRM',
@@ -117,31 +209,4 @@ function xdedupe_civicrm_navigationMenu(&$menu)
         ]
     );
     _xdedupe_civix_navigationMenu($menu);
-}
-
-/**
- * Helper function to see if the menu item is already there
- *
- * @param $menu array current menu
- * @param $path string path to look for
- *
- * @return bool
- */
-function _xdedupe_menu_exists(&$menu, $path) {
-    // Find an recurse into the next level down
-    $found = FALSE;
-    $path = explode('/', $path);
-    $first = array_shift($path);
-    foreach ($menu as $key => &$entry) {
-        if ($entry['attributes']['name'] == $first) {
-            if (empty($path)) {
-                return true;
-            }
-            $found = _xdedupe_menu_exists($entry['child'], implode('/', $path));
-            if ($found) {
-                return true;
-            }
-        }
-    }
-    return $found;
 }
