6699d2f5c4be4d26a00af8df086994d3cb37b164
Stefan Frank <frank@systopia.de>
2025-03-04 16:18:25 +0100

    [#27871] code compliance phpcs, phpstan (partly) - minor corrections

--- CRM/Xdedupe/Resolver/CustomGroupByField.php
+++ CRM/Xdedupe/Resolver/CustomGroupByField.php
@@ -14,164 +14,194 @@
 | written permission from the original author(s).        |
 +--------------------------------------------------------*/
 
+declare(strict_types = 1);
 use CRM_Xdedupe_ExtensionUtil as E;
 
 /**
  * Implements a resolver for contact custom fields
  */
-class CRM_Xdedupe_Resolver_CustomGroupByField extends CRM_Xdedupe_Resolver
-{
+class CRM_Xdedupe_Resolver_CustomGroupByField extends CRM_Xdedupe_Resolver {
 
-    /** @var integer ID of the custom field */
-    protected $custom_group_id = null;
-
-    public function __construct($merge, $custom_group_id)
-    {
-        $this->custom_group_id = $custom_group_id;
-        parent::__construct($merge);
-    }
+  /**
+   * @var integer ID of the custom group
+   * @required
+   * @phpstan-var integer
+   */
+  protected int $custom_group_id;
 
     /**
-     * Get the spec (i.e. class name) that refers to this resolver
-     * @return string spec string
+     * @param CRM_Xdedupe_Merge $merge
+     * @param integer $custom_group_id
      */
-    public function getSpec()
-    {
-        return "CRM_Xdedupe_Resolver_CustomGroupByField:{$this->custom_group_id}";
-    }
+  public function __construct($merge, $custom_group_id) {
+    $this->custom_group_id = $custom_group_id;
+    parent::__construct($merge);
+  }
 
-    /**
-     * Report the contact attributes that this resolver requires
-     *
-     * @return array list of contact attributes
-     */
-    public function getContactAttributes()
-    {
-        return ["custom_{$this->custom_group_id}"];
-    }
+  /**
+   * Get the spec (i.e. class name) that refers to this resolver
+   * @return string spec string
+   */
+  public function getSpec() {
+    return "CRM_Xdedupe_Resolver_CustomGroupByField:{$this->custom_group_id}";
+  }
 
-    /**
-     * get the name of the finder
-     * @return string name
-     */
-    public function getName()
-    {
-        $group_name = civicrm_api4('CustomGroup', 'get', [ 'select' => [ 'title',], 'where' => [ [ 'id', '=',  $this->custom_group_id ] , ], ]);
-        return E::ts("Merge by field for group '%1'", [ 1 =>  $group_name[0]['title']]);
-    }
+  /**
+   * Report the contact attributes that this resolver requires
+   *
+   * @return array<string> list of contact attributes
+   */
+  public function getContactAttributes() {
+    return ["id"];
+  }
 
-    /**
-     * get an explanation what the finder does
-     * @return string name
-     */
-    public function getHelp()
-    {
-        $field_name = civicrm_api4('CustomField', 'get', [ 'select' => [ 'label',], 'where' => [ [ 'id', '=', $this->custom_group_id ] , ], ]);
-        return E::ts(
-            "The field '%1' is a custom field. This resolver will merge the values of all duplicates. It will fill empty fields with the first found value and it will add new options to a multi select field..",
-            [1 => $field_name[0]['label']]
-        );
-    }
+  /**
+   * get the name of the finder
+   * @return string name
+   */
+  public function getName() {
+    $group_name = civicrm_api4(
+            'CustomGroup', 'get',
+            [
+              'select' => ['title'],
+              'where' => [['id', '=', $this->custom_group_id]],
+            ]
+    );
+    return E::ts("Merge by field for group '%1'", [1 => $group_name[0]['title']]);
+  }
 
-    /**
-     * Get the contact's field values
-     *
-     * @param $contact_id integer contact ID
-     * @return array
-     */
-    protected function getValues($contact_id)
-    {
-        // get group name
-        $group_name = civicrm_api4('CustomGroup', 'get', [ 'select' => [ 'name',], 'where' => [ [ 'id', '=', $this->custom_group_id ] , ], ]);
-        // get values for contact
-        $group_values =  civicrm_api4('Contact', 'get', ['select' => [ $group_name[0]['name'] . '.*',], 'where' => [['id', '=', $contact_id], ], ]);
+  /**
+   * get an explanation what the finder does
+   * @return string name
+   */
+  public function getHelp() {
+    $field_name = civicrm_api4(
+            'CustomField', 'get',
+            [
+              'select' => ['label'],
+              'where' => [['id', '=', $this->custom_group_id]],
+            ]
+    );
+    return E::ts(
+        "The field '%1' is a custom field. This resolver will merge the
+        values of all duplicates. It will fill empty fields with the first
+        found value and it will add new options to a multi select field..",
+        [1 => $field_name[0]['label']]
+    );
+  }
 
-        $values = $group_values[0];
+  /**
+   * Get the contact's field values
+   *
+   * @param integer $contact_id contact ID
+   * @return array<string> values custom group
+   */
+  protected function getValues($contact_id) {
+    // get group name
+    $group_name = civicrm_api4(
+            'CustomGroup', 'get',
+            [
+              'select' => ['name'],
+              'where' => [['id', '=', $this->custom_group_id]],
+            ]
+    );
+    // get values for contact
+    $group_values = civicrm_api4(
+            'Contact',
+            'get',
+            [
+              'select' => [$group_name[0]['name'] . '.*'],
+              'where' => [['id', '=', $contact_id]],
+            ]
+    );
 
-        // id is always set, so unset
-        unset($values['id']);
+    $values = $group_values[0];
 
-        return $values;
-    }
+    // id is always set, so unset
+    unset($values['id']);
 
-    /**
-     * Resolve the privacy conflicts by maintaining any opt-opt-outs
-     *
-     * @param $main_contact_id    int     the main contact ID
-     * @param $other_contact_ids  array   other contact IDs
-     * @return boolean TRUE, if there was a conflict to be resolved
-     * @throws Exception if the conflict couldn't be resolved
-     */
-    public function resolve($main_contact_id, $other_contact_ids)
-    {
-        $main_contact_values     = $this->getValues($main_contact_id);
-        // seems not to work
-        $this->addMergeDetail(
-                            E::ts("Merge by field group id: [%1]", [1 => $this->custom_group_id])
-                    );
-        $new_main_contact_values = $main_contact_values;
-        // collect values in array
-        foreach ($other_contact_ids as $other_contact_id) {
-            $other_contact_values      = $this->getValues($other_contact_id);
-            foreach ($new_main_contact_values as $key => $value) {
-                // merge array - add if empty
-                if(is_array($value)) {
-                    foreach ($other_contact_values[$key] as $v) {
-                        if(array_search($v, $value) === false) {
-                            $new_main_contact_values[$key][] = $v;
-                        }
-                    }
-                } else {
-                    // merge textfield
-                    if(empty($value)) {
-                        $new_main_contact_values[$key] = $other_contact_values[$key];
-                    }
-                }
+    return $values;
+  }
+
+  /**
+   * Resolve the privacy conflicts by maintaining any opt-opt-outs
+   *
+   * @param int $main_contact_id   the main contact ID
+   * @param array<int> $other_contact_ids  other contact IDs
+   * @return boolean TRUE, if there was a conflict to be resolved
+   */
+  public function resolve($main_contact_id, $other_contact_ids) {
+    $main_contact_values = $this->getValues($main_contact_id);
+    // seems not to work
+    $this->addMergeDetail(
+                        E::ts('Merge by field group id: [%1]', [1 => $this->custom_group_id])
+                );
+    $new_main_contact_values = $main_contact_values;
+    // collect values in array
+    foreach ($other_contact_ids as $other_contact_id) {
+      $other_contact_values = $this->getValues($other_contact_id);
+      foreach ($new_main_contact_values as $key => $value) {
+        // merge array - add if empty
+        // phpstan needs a hint
+        if (is_array($value)) {
+          foreach ($other_contact_values[$key] as $v) {
+            if (array_search($v, $value) === FALSE) {
+              $new_main_contact_values[$key][] = $v;
             }
+          }
         }
+        else {
+          // merge textfield
+          if (empty($value)) {
+            $new_main_contact_values[$key] = $other_contact_values[$key];
+          }
+        }
+      }
+    }
 
-        // now, perform the contact updates if necessary
-        $all_contact_ids = array_merge($other_contact_ids, [$main_contact_id]);
-        foreach ($all_contact_ids as $contact_id) {
+    // now, perform the contact updates if necessary
+    $all_contact_ids = array_merge($other_contact_ids, [$main_contact_id]);
+    foreach ($all_contact_ids as $contact_id) {
 
-            civicrm_api4('Contact', 'update', [
-              'values' => $new_main_contact_values,
-              'where' => [
-                ['id', '=', $contact_id],
+      civicrm_api4('Contact', 'update',
+              [
+                'values' => $new_main_contact_values,
+                'where' => [
+        ['id', '=', $contact_id],
+                ],
+                'checkPermissions' => FALSE,
+              ]);
+    }
+    return TRUE;
+  }
+
+  /**
+   * Add a resolver spec for each Multi-Select field to the list
+   * @param array<string> $list list of resolver specs
+   * @return void
+   */
+  public static function addAllResolvers(&$list) {
+    $contact_custom_groups = civicrm_api4(
+            'CustomGroup',
+            'get',
+            [
+              'select' => [
+                'id', 'title',
               ],
-              'checkPermissions' => FALSE,
-            ]);
-        }
-        return true;
+              'where' => [
+                    ['extends', 'IN', ['Contact', 'Individual', 'Household', 'Organization']],
+                    ['is_active', '=', TRUE],
+              ],
+            ]
+    );
+
+    if (count($contact_custom_groups) > 1 ) {
+      return;
     }
 
-
-    /**
-     * Add a resolver spec for each Multi-Select field to the list
-     * @param $list array list of resolver specs
-     */
-    public static function addAllResolvers(&$list)
-    {
-         $contact_custom_groups    = civicrm_api4(
-                'CustomGroup',
-                'get',
-                [
-                      'select' => [
-                        'id', 'title'
-                      ],
-                      'where' => [
-                        ['extends', 'IN', ['Contact', 'Individual', 'Household', 'Organization']],
-                        ['is_active', '=', TRUE],
-                      ],
-                ]
-        );
-
-        if (empty($contact_custom_groups)) {
-            return;
-        }
-
-        foreach ($contact_custom_groups as $custom_group) {
-            $list[] = "CRM_Xdedupe_Resolver_CustomGroupByField:{$custom_group['id']}";
-        }
+    foreach ($contact_custom_groups as $custom_group) {
+      $list[] = "CRM_Xdedupe_Resolver_CustomGroupByField:{$custom_group['id']}";
     }
+  }
+
 }
