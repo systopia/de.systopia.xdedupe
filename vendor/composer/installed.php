330ba7c1ae70b7c1ae23cabf9456efd776b80145
DDEV User <nobody@example.com>
2026-06-29 09:21:52 +0200

    Set version to 1.4.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.xdedupe',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '570ef5ff7f8b2a9f66a691f92149de0c858587cd',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.xdedupe' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '570ef5ff7f8b2a9f66a691f92149de0c858587cd',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
